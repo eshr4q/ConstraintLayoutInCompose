cac9b67f084acbd43d779da03a9bdb7124127521
eshr4q <feshr4q@gmail.com>
2023-11-27 14:50:37 +0330

    yay Bomb countdown

--- app/src/main/java/com/example/constraintlayoutincompose/MainActivity.kt
+++ app/src/main/java/com/example/constraintlayoutincompose/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.keyframes
 import androidx.compose.animation.core.spring
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -27,7 +28,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PaintingStyle.Companion.Stroke
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.semantics.Role.Companion.Button
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.TextUnit
@@ -39,11 +44,19 @@ import androidx.constraintlayout.compose.ConstraintSet
 import androidx.constraintlayout.compose.Dimension
 import androidx.constraintlayout.compose.layoutId
 import com.example.constraintlayoutincompose.ui.theme.ConstraintLayoutInComposeTheme
+import org.w3c.dom.Text
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            Box (
+                contentAlignment = Alignment.Center,
+                modifier = Modifier.fillMaxSize(),
+            ){
+                CircularProgressBar(percentage = 0.8f, number = 100 )
+
+            }
 
         }
     }
@@ -72,10 +85,28 @@ fun CircularProgressBar (
         )
     )
     LaunchedEffect(key1 =true){
-
+        animationPlayed = true
     }
 
-
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.size(radius * 2f)
+    ) {
+        Canvas(modifier = Modifier.size(radius * 2f)) {
+            drawArc(
+                color = color,
+                -90f,
+                360 * curPercentage.value,
+                useCenter = false,
+                style = Stroke(strokeWith.toPx(), cap = StrokeCap.Round)
+            )
+        }
+        Text(
+            text = (curPercentage.value * number).toInt().toString(),
+            color = Color.Black,
+            fontWeight = FontWeight.Bold,
+        )
+    }
 }
 
 
