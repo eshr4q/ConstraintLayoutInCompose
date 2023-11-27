479fdba5fdf114a3460aa9caee6966150f2846d8
eshr4q <feshr4q@gmail.com>
2023-11-27 13:32:32 +0330

    How to Make an Animated Circular Progress Bar in Jetpack Compose - Android Studio Tutorial

--- app/src/main/java/com/example/constraintlayoutincompose/MainActivity.kt
+++ app/src/main/java/com/example/constraintlayoutincompose/MainActivity.kt
@@ -6,6 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.animation.core.LinearOutSlowInEasing
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.keyframes
 import androidx.compose.animation.core.spring
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
@@ -16,13 +18,21 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.semantics.Role.Companion.Button
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ChainStyle
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.ConstraintSet
@@ -34,47 +44,88 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            var sizeState by remember { mutableStateOf(200.dp)}
-            val size by animateDpAsState(
-                targetValue = sizeState,
-               spring(
-                   Spring.DampingRatioHighBouncy )
-                )
-            Box (modifier = Modifier
-                .background(Color.Red)
-                .size(size),
-                contentAlignment = Alignment.Center){
-                Button(onClick = {
-                 sizeState += 50.dp
-               }) {
-                    Text ("increase size")
-                }
-            }
-        }
-
-
 
         }
     }
-
+}
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun CircularProgressBar (
+    percentage: Float,
+    number: Int,
+    textUnit: TextUnit = 28.sp,
+    radius: Dp = 50.dp,
+    color: Color = Color.Red,
+    strokeWith: Dp = 8.dp,
+    animDuration: Int = 1000,
+    animDelay: Int = 0,
+    ) {
+    var animationPlayed by remember {
+        mutableStateOf(false)
+    }
+    val curPercentage = animateFloatAsState (
+        targetValue = if (animationPlayed) percentage else 0f,
+        animationSpec = tween(
+            durationMillis = animDuration,
+            delayMillis = animDelay,
+
+        )
     )
-}
+    LaunchedEffect(key1 =true){
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ConstraintLayoutInComposeTheme {
-        Greeting("Android")
     }
+
+
 }
 
-// <ConstraintLayout>
+
+
+
+
+
+
+//        animated whatever ************************************************
+
+//            var sizeState by remember { mutableStateOf(200.dp)}
+//            val size by animateDpAsState(
+//                targetValue = sizeState,
+//                keyframes {  }
+//                )
+//            Box (modifier = Modifier
+//                .background(Color.Red)
+//                .size(size),
+//                contentAlignment = Alignment.Center){
+//                Button(onClick = {
+//                 sizeState += 50.dp
+//               }) {
+//                    Text ("increase size")
+//                }
+//            }
+//        }
+//
+//
+//
+//        }
+//    }
+//
+//
+//@Composable
+//fun Greeting(name: String, modifier: Modifier = Modifier) {
+//    Text(
+//        text = "Hello $name!",
+//        modifier = modifier
+//    )
+//}
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    ConstraintLayoutInComposeTheme {
+//        Greeting("Android")
+//    }
+//}
+
+// <ConstraintLayout>*****************************************************************************
 
 //    val constraints = ConstraintSet {
 //        val greenBox = createRefFor( "greenbox")
